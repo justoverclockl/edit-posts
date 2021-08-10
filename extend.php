bed69734f745c984b145b0e6354e228e33ae4975
Marco Colia <info@justoverclock.it>
2021-08-10 19:56:00 +0200

    1st working version

--- extend.php
+++ extend.php
@@ -4,6 +4,8 @@
  * This file is part of justoverclock/edit-posts.
  *
  * Copyright (c) 2021 Marco Colia.
+ * https://flarum.it
+ * based on Clark Winkelmann tutorial
  *
  * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,5 +1,16 @@
+/*
+ * This file is part of justoverclock/edit-posts.
+ *
+ * Copyright (c) 2021 Marco Colia.
+ * https://flarum.it
+ * based on Clark Winkelmann tutorial
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
 import CommentPost from 'flarum/forum/components/CommentPost';
-import {extend} from 'flarum/common/extend';
+import { extend } from 'flarum/common/extend';
 import app from 'flarum/forum/app';
 import Button from 'flarum/common/components/Button';
 import PostControls from 'flarum/forum/utils/PostControls';
